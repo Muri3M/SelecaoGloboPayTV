bdd6b4ea9baa240ee021c8ff5a454221f245edfb
Muri3M <mmmurilo@hotmail.com>
2022-08-22 16:09:53 -0300

    refactor: change reset css folder

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import ResetCss from "../components/atoms/ResetCss";
+import ResetCss from "../style/ResetCss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
